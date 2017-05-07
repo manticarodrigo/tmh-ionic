1fd6dd5d8c748474648954cc263c661c52c51d1c
Rodrigo Mantica <manticarodrigo@rodrigos-mbp.home>
2017-05-06 18:50:54 -0700

    Added CDVWKWebViewEngine

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,7 +24,7 @@ export class TheManHome {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
+      statusBar.styleLightContent();
       splashScreen.hide();
       // Fetch current user
       this.fetchCurrentUser();
