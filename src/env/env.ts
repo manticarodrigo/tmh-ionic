ee0ba1d418fb2a832ef3878e9064da7561141cb0
manticarodrigo <manticarodrigo@gmail.com>
2018-09-02 12:54:06 -0400

    env vars

--- src/env/env.ts
+++ src/env/env.ts
@@ -1,6 +1,7 @@
 export const ENV = {
   production: false,
   // backendUrl: 'https://tmh-django.herokuapp.com',
+  // socketUrl: 'https://themanhome.herokuapp.com',
   backendUrl: 'http://localhost:8000',
-  socketUrl: 'http://localhost:3000'
+  socketUrl: 'http://localhost:3000',
 };
